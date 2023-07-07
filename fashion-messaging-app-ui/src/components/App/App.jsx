c308d1d3b99439870f2d26fff68cdadc2466629c
preciousonah <preciousonah12@gmail.com>
2023-07-07 15:51:07 -0700

    Add the search feature

--- fashion-messaging-app-ui/src/components/App/App.jsx
+++ fashion-messaging-app-ui/src/components/App/App.jsx
@@ -1,39 +1,43 @@
-import "./App.css"
+import "./App.css";
 import Search from "../Search/Search";
 import Navbar from "../Navbar/Navbar";
 import FashionItems from "../FashionItems/FashionItems";
 import { useEffect, useState } from "react";
 import { createClient } from "pexels";
 
-const API_KEY = "06ZcwjgbmJBM8T2TxLUZ5iwdXXGxiAgz0Z018b7QPKwR1ExipkFjaAuw"
+const API_KEY = "06ZcwjgbmJBM8T2TxLUZ5iwdXXGxiAgz0Z018b7QPKwR1ExipkFjaAuw";
 const clientAPI = createClient(API_KEY);
-const query = "Fashion";
+const defaultQuery = "Fashion";
 const PHOTOS = 100;
 
 export default function App() {
   const [photos, setPhotos] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
   useEffect(() => {
-    clientAPI.photos.search({ query, per_page: PHOTOS })
+    const query = searchQuery || defaultQuery;
+    clientAPI.photos
+      .search({ query, per_page: PHOTOS })
       .then(response => {
         setPhotos(response.photos);
       })
       .catch(error => {
         console.error("Error fetching photos:", error);
       });
-  }, []);
+  }, [searchQuery]);
+
+  const handleSearch = query => {
+    setSearchQuery(query);
+  };
 
   return (
     <div>
       <div className="container">
-       <h1>Aesthetik.</h1>
-       <Search />
-       <Navbar />
-      </div>    
-      <FashionItems photos = {photos}/>   
-    </div> 
+      <Search onSearch={handleSearch} />
+        <h1>Aesthetik.</h1>
+        <Navbar />
+      </div>
+      <FashionItems photos={photos} />
+    </div>
   );
 }
-
-
-
-
