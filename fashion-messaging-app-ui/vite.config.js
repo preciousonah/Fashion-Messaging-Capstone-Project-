17e1c353e8ba790c744535b87e4932bc30444a0f
preciousonah <preciousonah12@gmail.com>
2023-07-10 16:28:52 -0700

    Committing changes before subtree add

--- fashion-messaging-app-ui/vite.config.js
+++ fashion-messaging-app-ui/vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "preciousonah/Fashion-Messaging-Capstone-Project-",
   plugins: [react()],
-  build: {
-    outDir: 'fashion-messaging-app-ui/dist',
-  },
 })
