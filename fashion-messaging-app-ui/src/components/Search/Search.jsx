d5cdf28a58dfabfc7263a0ecb3d4c4f3ae533ad4
preciousonah <preciousonah12@gmail.com>
2023-07-11 01:15:10 -0700

    Add the search functionality

--- fashion-messaging-app-ui/src/components/Search/Search.jsx
+++ fashion-messaging-app-ui/src/components/Search/Search.jsx
@@ -3,17 +3,30 @@ import React, { useState } from "react";
 import "./Search.css";
 import { FaSearch} from "react-icons/fa";
 
-export default function Search() {
+export default function Search({ onSearch }) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchInput = event => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    onSearch(searchQuery);
+  };
+
   return (
     <div className="searchBtn">
-      <form>
-        <input
-          className="searchInput"
-          type="text"
-          placeholder="Search for outfits!"   
-        />
-        <button type="submit" className="submitBtn"><FaSearch/></button>
-      </form>
+       <form onSubmit={handleSubmit}>
+      <input
+       className="searchInput"
+        type="text"
+        value={searchQuery}
+        onChange={handleSearchInput}
+        placeholder="Search your outfits!"
+      />
+      <button type="submit"className="submitBtn"><FaSearch/></button>
+    </form>
     </div>
   );
 }
