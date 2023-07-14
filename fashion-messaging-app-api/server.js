57535d891b4cd7018cc5d7fb53c3e44e096ff0f2
preciousonah <preciousonah12@gmail.com>
2023-07-14 14:40:20 -0700

    Add backend for user authentication

--- fashion-messaging-app-api/server.js
+++ fashion-messaging-app-api/server.js
@@ -1,11 +1,86 @@
-import express from "express";
+import express from 'express';
+import session from 'express-session';
+import cors from 'cors';
+import morgan from 'morgan';
+import { sequelize } from './database.js';
+import { User, Post } from './models/index.js';
+import userRoutes from './routes/users.js';
+import SequelizeStoreInit from 'connect-session-sequelize';
+
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.use(cors({
+  origin: 'http://localhost:3001',
+  credentials: true
+}));
+app.use(express.json()); 
+app.use(morgan())
+
+const SequelizeStore = SequelizeStoreInit(session.Store);
+const sessionStore = new SequelizeStore({
+  db: sequelize
 });
 
-app.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
+app.use(
+  session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {
+      sameSite: false,
+      secure: false,
+      expires: new Date(Date.now() + (365 * 24 * 60 * 60 * 1000)) 
+    }
+  })
+);
+sessionStore.sync();
+
+app.use(userRoutes);
+app.get('/posts', async (req, res) => {
+  try {
+    const posts = await Post.findAll({
+      include: [{ model: User, as: 'user' }],
+      order: [['createdAt', 'DESC']]
+    });
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
+
+
+app.post('/posts', async (req, res) => {
+  try {
+    if (!req.session.user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const currentUser = req.session.user;
+    const post = await Post.create({
+      ...req.body,
+      userId: currentUser.id
+    });
+
+    const postWithUser = await Post.findOne({
+      where: { id: post.id },
+      include: [{ model: User, as: 'user' }]
+    });
+
+    res.status(201).json(postWithUser);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+sequelize.sync({ alter: true })
+  .then(() => {
+    const port = 3000;
+    app.listen(port, () => {
+      console.log(`App is listening on port ${port}`);
+    });
+  })
+  .catch(error => {
+    console.error('Unable to connect to the database:', error);
+  });
+
