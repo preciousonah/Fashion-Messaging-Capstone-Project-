6e1fa3fd3bfef9289501464f592216fae2329e29
preciousonah <preciousonah12@gmail.com>
2023-07-21 20:15:21 -0700

    Add tooltip in the Navbar

--- fashion-messaging-app-ui/src/components/Homepage/Homepage.jsx
+++ fashion-messaging-app-ui/src/components/Homepage/Homepage.jsx
@@ -2,17 +2,17 @@ import Navbar from "../Navbar/Navbar";
 import FashionItems from "../FashionItems/FashionItems";
 import Search from "../Search/Search";
 import "./Homepage.css";
-
+import { Link } from "react-router-dom";
 
 function Homepage({photos, handleSearch}) {
   return (
     <div>
         <div className="container">
-
+        <Link to="/logout">Logout</Link>
             <Navbar />
             <h1>Aesthetik.</h1>
             <Search onSearch={handleSearch} />
-
+        
         </div>
             <FashionItems photos={photos} />
     </div>
