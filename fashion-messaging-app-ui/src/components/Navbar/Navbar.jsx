bbd663c42b4e1263b354bcbeb911d5931880c9e0
preciousonah <121525219+preciousonah@users.noreply.github.com>
2023-07-24 18:29:01 -0700

    Add tooltip in the Navbar (#16)

--- fashion-messaging-app-ui/src/components/Navbar/Navbar.jsx
+++ fashion-messaging-app-ui/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,26 @@
 import * as React from "react";
 import "./Navbar.css";
+import Tooltip from "../Tooltip/Tooltip";
 
 export default function Navbar() {
+  const links = [
+    { text: "Recommended Outfits!", url: "/", label: "Recommendations" },
+    { text: "Shop Ideas", url: "/", label: "Shopping" },
+    { text: "Open Messages", url: "/contact", label: "Messages" },
+    { text: "Make a Post!", url: "/post", label: "Post" },
+  ];
+
   return (
     <nav className="navbar">
-        <ul>
-          <li><a href="/">Profile</a></li>
-          <li><a href="/about">Notifications</a></li>
-          <li><a href="/contact">Messages</a></li>
-          <li><a href="/post">Post</a></li>
-        </ul>
-    </nav>
+    <ul className="navbar-list">
+      {links.map((link, index) => (
+        <li key={index} className="navbar-item">
+          <Tooltip text={link.text}>
+            <a href={link.url} className="navbar-link">{link.label}</a>
+          </Tooltip>
+        </li>
+      ))}
+    </ul>
+  </nav>
   );
 }
