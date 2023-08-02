4c178ca60b143d49cf3de4ff9292d7deabaa23c6
preciousonah <preciousonah12@gmail.com>
2023-08-02 05:05:55 -0700

    Update frontend to display source of information

--- fashion-messaging-app-ui/src/components/Recommendations/Recommendations.jsx
+++ fashion-messaging-app-ui/src/components/Recommendations/Recommendations.jsx
@@ -1,33 +1,21 @@
 
 import * as React from "react";
 
-export default function Recommendations( { recommendations } ) {
-    const { lastSearchPhotos, mostFrequentSearchPhotos } = recommendations;
-  
-    return (
-      <div className= "photo" >
-        <h2>Recommendations</h2>
-        <h3>Last Search Photos</h3>
-        {lastSearchPhotos.length === 0 ? (
-          <p>No last search photos at the moment.</p>
-        ) : (
-          lastSearchPhotos.map(photo => (
-            <div key={photo.id}>
-              <img src={photo.src.original} alt="" />
-            </div>
-          ))
-        )}
-        <h3>Most Frequent Search Photos</h3>
-        {mostFrequentSearchPhotos.length === 0 ? (
-          <p>No most frequent search photos at the moment.</p>
-        ) : (
-          mostFrequentSearchPhotos.map(photo => (
-            <div key={photo.id}>
-              <img src={photo.src.original} alt="" />
-            </div>
-          ))
-        )}
-      </div>
-    );
-  }
-  
+export default function Recommendations({ recommendations }) {
+  return (
+    <div className="photo">
+      <h1>Welcome to the recommendations page!!</h1>
+      <h2>Recommendations</h2>
+      {recommendations.length === 0 ? (
+        <p>No recommendations at the moment.</p>
+      ) : (
+        recommendations.map(photo => (
+          <div key={photo.id}>
+            <img src={photo.src.original} alt="" />
+            <p>{`Source: ${photo.source}`}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
