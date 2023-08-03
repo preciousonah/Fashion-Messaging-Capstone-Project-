c46ae48d1cc9847d005ab8d13b0dbcfac4857f73
preciousonah <preciousonah12@gmail.com>
2023-08-02 23:32:38 -0700

    display each recommendation array in frontend

--- fashion-messaging-app-ui/src/components/Recommendations/Recommendations.jsx
+++ fashion-messaging-app-ui/src/components/Recommendations/Recommendations.jsx
@@ -1,21 +1,31 @@
 
-import * as React from "react";
+import React from 'react';
+import './Recommendations.css';
 
 export default function Recommendations({ recommendations }) {
-  return (
-    <div className="photo">
-      <h1>Welcome to the recommendations page!!</h1>
-      <h2>Recommendations</h2>
-      {recommendations.length === 0 ? (
-        <p>No recommendations at the moment.</p>
-      ) : (
-        recommendations.map(photo => (
-          <div key={photo.id}>
-            <img src={photo.src.original} alt="" />
-            <p>{`Source: ${photo.source}`}</p>
+    if (!recommendations) {
+      return <p>Loading recommendations...</p>;
+    }
+  
+    const headers = {
+      "lastSearch": "Psst, you were searching for these, would you like to see them again?",
+      "mostFrequent": "You've been looking at these a lot, check these other ones",
+      "posts": "Based on your recent posts...",
+      "popularSearch": "Look at some of the outfits the other users are looking at, wanna check them out?"
+    };
+  
+    return (
+      <div className="recommendations">
+        {Object.keys(recommendations).map(key => (
+          <div className="key" key={key}>
+            <h2>{headers[key] || key}</h2>
+            {recommendations[key].map((photo, index) => (
+              <div className='photo' key={index}>
+                   <img src={photo.src.original} alt={photo.title} />
+              </div>
+            ))}
           </div>
-        ))
-      )}
-    </div>
-  );
-}
+        ))}
+      </div>
+    );
+  }
