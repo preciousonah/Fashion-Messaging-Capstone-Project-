40ee4e209e5d515ff48d39c88c53cd78ed2ab9fe
preciousonah <121525219+preciousonah@users.noreply.github.com>
2023-08-04 10:57:38 -0700

    Homepage UI add (#32)
    * Add styling to the search bar
    
    * Remove Tooltip text and functionality from Navbar
    
    * Add more css styling in the navigation bar
    
    * Move website name position in navbar
    
    * Add css styling to homepage
    
    * Add more images features on homepage
    
    * Add more visual styling to the image display part of homepage

--- fashion-messaging-app-ui/src/components/Navbar/Navbar.jsx
+++ fashion-messaging-app-ui/src/components/Navbar/Navbar.jsx
@@ -1,26 +1,16 @@
 import * as React from "react";
 import "./Navbar.css";
-import Tooltip from "../Tooltip/Tooltip";
+
 
 export default function Navbar() {
-  const links = [
-    { text: "Recommended Outfits!", url: "/recommendations", label: "Recommendations" },
-    { text: "Shop Ideas", url: "/", label: "Shopping" },
-    { text: "Open Messages", url: "/contact", label: "Messages" },
-    { text: "Make a Post!", url: "/post", label: "Post" },
-  ];
-
   return (
     <nav className="navbar">
-    <ul className="navbar-list">
-      {links.map((link, index) => (
-        <li key={index} className="navbar-item">
-          <Tooltip text={link.text}>
-            <a href={link.url} className="navbar-link">{link.label}</a>
-          </Tooltip>
-        </li>
-      ))}
-    </ul>
-  </nav>
+      
+        <ul>
+          <li><a href="/recommendations">Recommendations</a></li>
+          <li><a href="/post">Post</a></li>
+          <li><a href="/">Home</a></li>
+        </ul>
+    </nav>
   );
 }
