fea0c0535be52235d99d30beb5d2a75b6592cc3f
preciousonah <preciousonah12@gmail.com>
2023-08-04 09:39:24 -0700

    Add more images features on homepage

--- fashion-messaging-app-ui/src/components/FashionItems/FashionItems.jsx
+++ fashion-messaging-app-ui/src/components/FashionItems/FashionItems.jsx
@@ -3,13 +3,19 @@ import * as React from "react";
 import "./FashionItems.css";
 
 export default function Outfits({ photos, noResults }) {
-    return (
-      <div className="photo">
-        {noResults ? <p>No results</p> : 
-          photos.map(photo => (
-            <img key={photo.id} src={photo.src.original} alt="" />
-          ))
-        }
-      </div>
-    );
+  return (
+    <div className="photo-grid">
+      {noResults ? <p>No results</p> : 
+        photos.map(photo => (
+          <div key={photo.id} className="photo-item">
+            <div className="photo-wrap">
+              <img className= "photo-each"src={photo.src.original} alt="No pictures to show:(" />
+              <a href={photo.photographer_url} className="photo-photographer">{photo.photographer}</a>
+            </div>
+            <p className="photo-alt">{photo.alt}</p>
+          </div>
+        ))
+      }
+    </div>
+  );
 }
