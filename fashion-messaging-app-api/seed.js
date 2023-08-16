c2971a5df805c84f6572ff503146fa0ec9c89105
preciousonah <preciousonah12@gmail.com>
2023-08-16 14:15:16 -0700

    seed save-images data

--- fashion-messaging-app-api/seed.js
+++ fashion-messaging-app-api/seed.js
@@ -2,7 +2,7 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { User, Post, SearchResult } from './models/index.js';
+import { User, Post, SearchResult, SavedImage } from './models/index.js';
 import { sequelize } from './database.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -11,6 +11,7 @@ const __dirname = path.dirname(__filename);
 const userData = JSON.parse(fs.readFileSync(path.resolve(__dirname, './seeders/users.json'), 'utf8'));
 const postData = JSON.parse(fs.readFileSync(path.resolve(__dirname, './seeders/posts.json'), 'utf8'));
 const searchResultData = JSON.parse(fs.readFileSync(path.resolve(__dirname, './seeders/searchResults.json'), 'utf8'));
+const savedImageData = JSON.parse(fs.readFileSync(path.resolve(__dirname, './seeders/savedImages.json'), 'utf8'));
 
 const seedDatabase = async () => {
   try {
@@ -18,6 +19,7 @@ const seedDatabase = async () => {
     await User.bulkCreate(userData);
     await Post.bulkCreate(postData);
     await SearchResult.bulkCreate(searchResultData);
+    await SavedImage.bulkCreate(savedImageData);
   } catch (error) {
     console.error('Error seeding data:', error);
   } finally {
