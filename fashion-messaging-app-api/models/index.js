497498b8818ee90b0ed667155945f89af38af4d6
preciousonah <preciousonah12@gmail.com>
2023-08-16 14:16:33 -0700

    Add save-images model in database

--- fashion-messaging-app-api/models/index.js
+++ fashion-messaging-app-api/models/index.js
@@ -1,7 +1,8 @@
 
 import { User } from './user.js';
 import { Post } from './post.js';
-import { SearchResult } from './searchResult.js'; 
+import { SearchResult } from './searchResult.js';
+import { SavedImage } from './savedImage.js'; 
 
 User.hasMany(Post, { as: 'posts', foreignKey: 'userId' });
 Post.belongsTo(User, { as: 'user', foreignKey: 'userId' });
@@ -9,4 +10,7 @@ Post.belongsTo(User, { as: 'user', foreignKey: 'userId' });
 User.hasMany(SearchResult, { as: 'searchResults', foreignKey: 'userId' }); 
 SearchResult.belongsTo(User, { as: 'user', foreignKey: 'userId' }); 
 
-export { User, Post, SearchResult }; 
+User.hasMany(SavedImage, { as: 'savedImages', foreignKey: 'userId' });
+SavedImage.belongsTo(User, { as: 'user', foreignKey: 'userId' });
+
+export { User, Post, SearchResult, SavedImage };
